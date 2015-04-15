8bf6abd35f681a05909b8b5a02f439cb0c2f8cf5
Jeff <jeffchan35@gmail.com>
2015-04-15 15:54:22 +0800

    Add freetype ios version.

---
+++ Freetype/freetype-2.5.5/builds/iOS/Freetype2Lib/Freetype2Lib/Freetype2Lib.m
@@ -0,0 +1,13 @@
+//
+//  Freetype2Lib.m
+//  Freetype2Lib
+//
+//  Created by JeffChang on 2015/4/14.
+//  Copyright (c) 2015年 JeffChang. All rights reserved.
+//
+
+#import "Freetype2Lib.h"
+
+@implementation Freetype2Lib
+
+@end
